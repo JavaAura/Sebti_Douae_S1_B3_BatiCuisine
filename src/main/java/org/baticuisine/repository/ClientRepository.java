e85393c5edb608deebafeaa18ea18a753beef25a
Douae sb <douae.sb411@example.com>
2024-09-17 19:58:28 +0100

    client repo

--- src/main/java/org/baticuisine/repository/ClientRepository.java
+++ src/main/java/org/baticuisine/repository/ClientRepository.java
@@ -1,4 +1,9 @@
 package org.baticuisine.repository;
 
+import org.baticuisine.entities.Client;
+
 public interface ClientRepository {
+    void addClient(Client client);
+    Client getClientById(int clientId);
+
 }
