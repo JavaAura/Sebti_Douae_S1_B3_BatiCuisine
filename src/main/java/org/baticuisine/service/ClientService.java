b8e6705841cf13364e6a4e0686d00717566c95a6
Douae sb <douae.sb411@example.com>
2024-09-18 16:11:19 +0100

    client service

---
+++ src/main/java/org/baticuisine/service/ClientService.java
@@ -0,0 +1,11 @@
+package org.baticuisine.service;
+
+import org.baticuisine.entities.Client;
+
+public interface ClientService {
+    void addClient(Client client);
+
+    Client getClientById(int clientId);
+
+    Client searchClientByName(String name);
+}
---
+++ src/main/java/org/baticuisine/serviceImpl/ClientServiceImpl.java
@@ -0,0 +1,32 @@
+package org.baticuisine.serviceImpl;
+
+import org.baticuisine.service.ClientService;
+import org.baticuisine.entities.Client;
+import org.baticuisine.repository.ClientRepository;
+import org.baticuisine.repositoryImpl.ClientRepositoryImpl;
+
+
+    public class ClientServiceImpl implements ClientService {
+
+        private ClientRepository clientRepository;
+
+        public ClientServiceImpl() {
+            this.clientRepository = new ClientRepositoryImpl();
+        }
+
+        @Override
+        public void addClient(Client client) {
+            clientRepository.addClient(client);
+        }
+
+        @Override
+        public Client getClientById(int clientId) {
+            return clientRepository.getClientById(clientId);
+        }
+
+        @Override
+        public Client searchClientByName(String name) {
+            return clientRepository.searchClientByName(name);
+        }
+    }
+
